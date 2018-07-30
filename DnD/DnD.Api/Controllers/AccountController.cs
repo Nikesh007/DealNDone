2df6e137531dc5e0cc633afbd8175da84bb997ee
Nikesh007 <nikesh1408@gmail.com>
2018-07-30 22:15:03 +0530

    Login Api

---
+++ DnD/DnD.Api/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using DnD.Api.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DnD.Api.Controllers
+{
+    public class AccountController : ApiController
+    {
+        // GET: Account
+        [Route("api/User/Register")]
+        [HttpPost]
+        public IdentityResult Register(AccountModel model)
+        {
+            try
+            {
+                model.UserName = "nik";
+                model.Email = "nikesh1408@gmail.com";
+                model.Password = "nik";
+                var userStore = new UserStore<ApplicationUser>(new ApplicationDbContext());
+                var manager = new UserManager<ApplicationUser>(userStore);
+                var user = new ApplicationUser() { UserName = model.UserName, Email = model.Email };
+                //user.FirstName = model.FirstName;
+                //user.LastName = model.LastName;
+                manager.PasswordValidator = new PasswordValidator
+                {
+                    RequiredLength = 3
+                };
+                IdentityResult result = manager.Create(user, model.Password);
+
+
+
+                //if (result != null)
+
+                //{
+                //    TestRepository tst = new TestRepository();
+                //    tst.InsertUser(model.UserName);
+                //}
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return new IdentityResult();
+            }
+
+        }
+
+        [Route("api/User/GetUserList")]
+        [HttpGet]
+        public dynamic GetUserList()
+        {
+            //TestRepository tst = new TestRepository();
+            //tst.GetUserList();
+            return null;
+        }
+    }
+}
---
+++ DnD/DnD.Api/Models/IdentityModels.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DnD.Api.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>().ToTable("User");
+            modelBuilder.Entity<IdentityRole>().ToTable("Role");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("UserRole");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("UserClaim");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("UserLogin");
+
+        }
+    }
+}
